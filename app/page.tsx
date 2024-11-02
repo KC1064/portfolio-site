ec110ba0aad19412e862d650134c942b5d4396c1
KC1064 <kiron08072003@gmail.com>
2024-11-03 01:47:36 +0530

    Fix:Padding

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import TechStack from "./utils/TechStack";
 
 export default function Home() {
   return (
-    <div className="w-full lg:w-[60%] lg:px-6 lg:py-8 md:px-6 md:py-8 px-1 py-3  overflow-x-hidden flex flex-col justify-between gap-4">
+    <div className="w-full lg:w-[60%] lg:px-6 lg:py-8 md:px-6 md:py-8 px-4 py-4  overflow-x-hidden flex flex-col justify-between gap-4">
       <Navbar />
       <ProfileCard />
       <Projects />
